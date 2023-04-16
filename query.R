4447c3214f1ba2b7b94ef75a4fc21aca2ea515c7
TotalNoname <lemur0b@gmail.com>
2023-04-17 00:43:11 +0200

    1.0 rc2 zmiany w komentarzach do pliku

--- query.R
+++ query.R
@@ -1,11 +1,13 @@
 library(sqldf)
 library(ggplot2)
 
-### DLA MODUŁU KONKURSOWEGO
+
+
+##### DLA MODUŁU KONKURS
 
 contest_query = function(path_contest)
 {
-  ###nie modyfikować bez odpowiedniej wiedzy
+  ###nie modyfikować 
   content = read.csv(path_contest)
   content = content[-1]
   ###
@@ -14,15 +16,18 @@ contest_query = function(path_contest)
   
   ##PONIŻEJ PRZYKŁADOWE ZAPYTANIA
   
+  
   ##wyświetla wszystkich zakwalifikowanych zawodników
   
   #query = sqldf("select * from content where [Suma.punktow] != 'Tq' 
   #order by Miejsce")
   
+  
   ##wyświetla wszystkich zakwalifikowanych zawodników z Polski
   
   query = sqldf("select * from content where Narodowość = 'POL' and 
                [Suma.punktow] != 'Tq' order by Miejsce")
+  
   
   ##zlicza ilość zawodników którzy urodzili się w danym roku
   
@@ -30,8 +35,12 @@ contest_query = function(path_contest)
                 #count([Rok.Urodzenia]) as 'Liczba zawodników urodzona w danym roku' from content 
                 #where [Suma.punktow] != 'Tq' group by [Rok.Urodzenia] order by count([Rok.Urodzenia]) desc")
   
+  
+  ###### ZWRACANIE PYTANIA
   return(query)
 }
+
+### DANE WYKRESOWE
 
 os1 = function()
 {
@@ -40,17 +49,24 @@ os1 = function()
   
   x1 = aes(x=Suma.punktow, y=Miejsce)
   
+  
   #dane wykresowe dla trzeciego zapytania
   
   #x1 = aes(x=`Rok urodzenia`, y=`Liczba zawodników urodzona w danym roku`)
+  
+  
+  ##### ZWRACANIE DANYCH WYKRESOWYCH
   return(x1)
 }
 
-### DLA MODUŁU ZAWODNIKÓW
+
+
+
+##### DLA MODUŁU ZAWODNICY
 
 contestant_query = function(link)
 {
-  ###nie modyfikować bez odpowiedniej wiedzy
+  ###nie modyfikować
   page = read_html(link)
   data = page %>% html_elements("table#ctl00_MainContent_gvKonkursyMie") 
   dane = html_table(data)
@@ -61,14 +77,16 @@ contestant_query = function(link)
   
   ##PONIŻEJ PRZYKŁADOWE ZAPYTANIA
   
-  #wyświetlanie osiągniętych miejsc przez zawodnika w PŚ w sezonie 2022/2023
-  # w konkursach indywidualnych
+  
+  #wyświetlanie osiągniętych miejsc przez zawodnika w każdym konkursie indywidualnym 
+  #PŚ w sezonie 2022/2023
   
   graph = paste("select * from dane where Miejsce is not NULL
                       and [Typ.zawodów] like 'PS%'
                       and [Typ.zawodów] not like '%Dr%'
                       and [Typ.zawodów] not like '%SuTe%'
                       and [Data.Konkursu] between '2022.11.05' and '2023.04.02'")
+  
   
   #wyświetlanie ilości osiągniętych miejsc w przekroju całej kariery w PŚ dla danego zawodnika
   # w konkursach indywidualnych
@@ -80,9 +98,13 @@ contestant_query = function(link)
                 #and [Typ.zawodów] not like '%SuTe%'
                 #group by Miejsce order by Miejsce asc")
   
+  
+  ###### ZWRACANIE PYTANIA
   query = sqldf(graph)
   return(query)
 }
+
+### DANE WYKRESOWE
 
 os2 = function()
 {
@@ -90,9 +112,12 @@ os2 = function()
   
   x2 = aes(x=Data.Konkursu, y=Miejsce)
   
+  
   #dane wykresowe dla drugiego zapytania
   
   #x2 = aes(x=Miejsce, y=`Liczba osiągniętych miejsc`)
   
+  
+  ##### ZWRACANIE DANYCH WYKRESOWYCH
   return(x2)
 }
